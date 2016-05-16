50d535bd46d41a633cb9c5b534e9e0870158cd9d
jamesyburr <vint.james@gmail.com>
2016-05-16 12:13:03 -0400

    Initial import

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jamesyburr on 5/16/16.
+ */
+public class Animal {
+    String name;
+    String species;
+    double weight;
+}
+
---
+++ src/Person.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jamesyburr on 5/16/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+    Person[] family = new Person [10];
+}
