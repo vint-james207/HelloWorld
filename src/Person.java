2087279f8c30f476642a6725ff95ba567cba91dc
jamesyburr <vint.james@gmail.com>
2016-05-17 11:58:14 -0400

    Add constructors and methods

--- src/Person.java
+++ src/Person.java
@@ -2,8 +2,46 @@
  * Created by jamesyburr on 5/16/16.
  */
 public class Person {
+    static int familySize = 10;
     String name;
     int age;
     boolean isAlive;
-    Person[] family = new Person [10];
+    Person[] family = new Person [familySize];
+
+    public Person(String name, int age, boolean isAlive) {
+        this.name = name;
+        this.age = age;
+        this.isAlive = isAlive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        if (isValidName (newName)) {
+            name = newName;
+        }
+    }
+
+    public int getAge () {
+        return age;
+    }
+
+    public void setAge (int newAge) {
+        age = newAge;
+    }
+
+    public boolean getIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive (boolean newIsAlive) {
+        isAlive = newIsAlive;
+    }
+
+    public boolean isValidName(String newName) {
+        return newName.contains(" ");
+    }
 }
+
